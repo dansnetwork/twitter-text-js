6e709b2f7e4da8e284813594fbd2e519f849a281
Jacob Gable <jacob.gable@gmail.com>
2013-01-16 10:43:09 -0600

    Fixing trailing slash on test

--- test/tests.js
+++ test/tests.js
@@ -271,7 +271,7 @@ test("twttr.txt.autolink", function() {
   var testUrl = "http://twitter.com?var=@val";
   same(twttr.txt.autoLink(testUrl),  "<a href=\"" + testUrl + "\" rel=\"nofollow\">" + testUrl + "</a>", "Autolink with special char params");
   // handle the @ character in the URL and an @mention at the same time
-  same(twttr.txt.autoLink(testUrl + " @mention"),  "<a href=\"" + testUrl + "\" rel=\"nofollow\">" + testUrl + "</a> @<a class=\"tweet-url username\" href=\"https://twitter.com/mention\" data-screen-name=\"mention\" rel=\"nofollow\">mention</a>", "Autolink with special char params and mentions");
+  same(twttr.txt.autoLink(testUrl + " @mention"),  "<a href=\"" + testUrl + "\" rel=\"nofollow\">" + testUrl + "</a> @<a class=\"tweet-url username\" href=\"https://twitter.com/mention/\" data-screen-name=\"mention\" rel=\"nofollow\">mention</a>", "Autolink with special char params and mentions");
 });
 
 test("twttr.txt.linkTextWithEntity", function() {
